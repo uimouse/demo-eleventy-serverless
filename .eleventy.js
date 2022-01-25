b4cc7223ea6680d45fe2052cf72554f782fe3fb5
uiMouse <69107617+uimouse@users.noreply.github.com>
2022-01-25 10:18:42 +0630

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+const fs = require("fs");
+
+module.exports = function(eleventyConfig) {
+	// Render on first-request
+	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+		name: "serverless",
+		functionsDir: "./netlify/functions/",
+	});
+
+	// Fully dynamic template for comparison
+	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+		name: "dynamic",
+		functionsDir: "./netlify/functions/",
+	});
+
+	// testing
+	eleventyConfig.addFilter("dateDebug", inputPath => {
+		return fs.statSync(inputPath);
+	})
+
+	return {
+		dir: {
+			input: "src"
+		}
+	};
+};
